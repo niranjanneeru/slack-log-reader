ed69f6e3e8d61674f9c31502aec89f5945f0e033
sameem-kv <sameem@keyvalue.systems>
2023-10-18 18:02:56 +0530

    command line args parser added

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
+import argparse
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from datetime import datetime
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 def to_timestamp(date_string):
-    date_format = "%d-%m-%Y %H-%M-%S"
+    date_format = "%d-%m-%Y %H:%M:%S"
     date_obj = datetime.strptime(date_string, date_format)
     return int(date_obj.timestamp())
+
 
 def parse_text(text):
     if not text.startswith("Incident"):
@@ -20,8 +21,9 @@ def parse_text(text):
     rest = lines[0].split(" ")
     action = rest[0]
     link, link_id = rest[1][1:-1].split("|")
-    process = 'stopped' if rest[2] == 'stopped' else 'running'
+    process = "stopped" if rest[2] == "stopped" else "running"
     return action, link, link_id, process
+
 
 def get_all_messages():
     result = client.conversations_history(
@@ -29,46 +31,52 @@ def get_all_messages():
         oldest=timestamp_from,
         latest=timestamp_to,
         limit=200,
-        inclusive=True
+        inclusive=True,
     )
 
     messages = result["messages"]
     latest_message = messages[-1]
 
-    while result['has_more']:
+    while result["has_more"]:
         result = client.conversations_history(
-        channel=channel_id,
-        oldest=timestamp_from,
-        latest=latest_message['ts'],
-        limit=200,
-        inclusive=True)
+            channel=channel_id,
+            oldest=timestamp_from,
+            latest=latest_message["ts"],
+            limit=200,
+            inclusive=True,
+        )
 
-       
         latest_message = result["messages"][-1]
-       
+
         for m in result["messages"]:
             messages.append(m)
 
     return messages
 
 
+parser = argparse.ArgumentParser(
+    description="A simple python script to summarise prod alert logs in slack with command-line arguments."
+)
+parser.add_argument("--begin", help="Format is DD-MM-YYYY HH:MM:SS (24 hr format)")
+parser.add_argument("--end", help="Format is DD-MM-YYYY HH:MM:SS (24 hr format)")
+args = parser.parse_args()
+from_time = args.begin
+to_time = args.end
 
-client = WebClient(token=os.environ('SLACK_API_KEY'))
+client = WebClient(token=os.environ["SLACK_API_KEY"])
 
-channel_id = os.environ('LOG_CHANNEL_ID')
-
-timestamp_from = to_timestamp("17-10-2023 14-10-00")
-timestamp_to = to_timestamp("18-10-2023 14-10-00")
-
-
+channel_id = os.environ["LOG_CHANNEL_ID"]
 
 try:
-    
+    timestamp_from = to_timestamp(from_time)
+    timestamp_to = to_timestamp(to_time)
+except Exception as e:
+    print("Invalid arguments, please use python3 main.py --help")
+    exit(1)
 
+try:
     messages = get_all_messages()
-
-    print(len(messages))
-
+    print("\n--TOTAL MESSAGES DURING THIS TIMEFRAME: ", len(messages), "\n")
     incidents = {}
 
     for message in messages[::-1]:
@@ -76,15 +84,16 @@ try:
             action, link, link_id, process = parse_text(message["text"])
             if action == "Incident":
                 if process == "running":
-                    incidents[link_id] = (link)
+                    incidents[link_id] = link
                 if process == "stopped":
                     try:
                         del incidents[link_id]
                     except:
                         pass
-    
-    print(incidents)
-    print(len(incidents))
+
+    for k, v in incidents.items():
+        print("ID: ", k, " Link: ", v)
+    print("\n--NUMBER OF CRITICAL ALERTS PENDING: ", len(incidents), "\n")
 
 except SlackApiError as e:
     print(f"Error: {e.response['error']}")
