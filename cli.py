849f30a24710afcfa3da5334fd9a244584cd9777
Niranjan B <niranjannb7777@gmail.com>
2023-10-19 10:30:13 +0530

    feat: module wise

--- cli.py
+++ cli.py
@@ -2,56 +2,10 @@ import os
 import argparse
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from datetime import datetime
+from src import utils
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
-def to_timestamp(date_string):
-    date_format = "%d-%m-%Y %H:%M:%S"
-    date_obj = datetime.strptime(date_string, date_format)
-    return int(date_obj.timestamp())
-
-
-def parse_text(text):
-    if not text.startswith("Incident"):
-        return None, None, None, None
-    lines = text.split("\n")
-    rest = lines[0].split(" ")
-    action = rest[0]
-    link, link_id = rest[1][1:-1].split("|")
-    process = "stopped" if rest[2] == "stopped" else "running"
-    return action, link, link_id, process
-
-
-def get_all_messages():
-    result = client.conversations_history(
-        channel=channel_id,
-        oldest=timestamp_from,
-        latest=timestamp_to,
-        limit=200,
-        inclusive=True,
-    )
-
-    messages = result["messages"]
-    latest_message = messages[-1]
-
-    while result["has_more"]:
-        result = client.conversations_history(
-            channel=channel_id,
-            oldest=timestamp_from,
-            latest=latest_message["ts"],
-            limit=200,
-            inclusive=True,
-        )
-
-        latest_message = result["messages"][-1]
-
-        for m in result["messages"]:
-            messages.append(m)
-
-    return messages
 
 
 parser = argparse.ArgumentParser(
@@ -63,13 +17,9 @@ args = parser.parse_args()
 from_time = args.begin
 to_time = args.end
 
-client = WebClient(token=os.environ["SLACK_API_KEY"])
-
-channel_id = os.environ["LOG_CHANNEL_ID"]
-
 try:
-    timestamp_from = to_timestamp(from_time)
-    timestamp_to = to_timestamp(to_time)
+    timestamp_from = utils.to_timestamp(from_time)
+    timestamp_to = utils.to_timestamp(to_time)
 except Exception as e:
     print("Invalid arguments, please use python3 main.py --help")
     exit(1)
@@ -77,14 +27,19 @@ except Exception as e:
 result = ""
 
 try:
-    messages = get_all_messages()
+    channel_id = os.environ["LOG_CHANNEL_ID"]
+    client = WebClient(token=os.environ["SLACK_API_KEY"])
+    
+    messages = utils.get_all_messages(client, timestamp_from, timestamp_to, channel_id)
+    
     print("\n--TOTAL MESSAGES DURING THIS TIMEFRAME: ", len(messages), "\n")
     result += f"TIMEFRAME: {from_time} to {to_time}\nTOTAL ALERTS: {len(messages)}\n\n"
+    
     incidents = {}
 
     for message in messages[::-1]:
         if "text" in message:
-            action, link, link_id, process = parse_text(message["text"])
+            action, link, link_id, process = utils.parse_text(message["text"])
             if action == "Incident":
                 if process == "running":
                     incidents[link_id] = link
---
+++ src/utils.py
@@ -0,0 +1,50 @@
+import os
+import argparse
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from datetime import datetime
+
+def to_timestamp(date_string):
+    date_format = "%d-%m-%Y %H:%M:%S"
+    date_obj = datetime.strptime(date_string, date_format)
+    return int(date_obj.timestamp())
+
+
+def parse_text(text):
+    if not text.startswith("Incident"):
+        return None, None, None, None
+    lines = text.split("\n")
+    rest = lines[0].split(" ")
+    action = rest[0]
+    link, link_id = rest[1][1:-1].split("|")
+    process = "stopped" if rest[2] == "stopped" else "running"
+    return action, link, link_id, process
+
+
+def get_all_messages(client, timestamp_from, timestamp_to, channel_id):
+    result = client.conversations_history(
+        channel=channel_id,
+        oldest=timestamp_from,
+        latest=timestamp_to,
+        limit=200,
+        inclusive=True,
+    )
+
+    messages = result["messages"]
+    latest_message = messages[-1]
+
+    while result["has_more"]:
+        result = client.conversations_history(
+            channel=channel_id,
+            oldest=timestamp_from,
+            latest=latest_message["ts"],
+            limit=200,
+            inclusive=True,
+        )
+
+        latest_message = result["messages"][-1]
+
+        for m in result["messages"]:
+            messages.append(m)
+
+    return messages
