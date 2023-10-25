caa61e316bf22a30887e6dd9cd04354172c15662
Niranjan B <niranjannb7777@gmail.com>
2023-10-25 09:52:58 +0530

    fix: action error

--- src/slack.py
+++ src/slack.py
@@ -23,7 +23,8 @@ def work(timestamp_from, timestamp_to, from_time, to_time):
         for message in messages[::-1]:
             if "text" in message:
                 action, link, link_id, process = utils.parse_text(message["text"])
-                details = message["attachments"][0]["text"] if "attachments" in message else None
+                details = message["attachments"][0] if "attachments" in message else None
+                details = details["text"] if "text" in message else None
                 timestamp = int(float(message['ts']))
                 if action == "Incident":
                     if process == "running":
